f3aa501e46b2ef3325d23aaac9a2a006b9e99356
AAteeb Khalid <ateebcancer@gmail.com>
2019-06-23 13:33:00 -0700

    while loop

---
+++ while.py
@@ -0,0 +1,9 @@
+while True:
+    print("Press 1 to add student")
+    print("Press 2 to delete student")
+    print("Press 3 to search student")
+    print("Press 4 to edit student")
+    print("Press 5 to Exit")
+    option = input("ENter 1-5:")
+    if option =='5':
+        break 
